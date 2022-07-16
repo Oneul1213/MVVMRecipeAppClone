3026156da1d1065a48d798382c3ce2885012b961
Oneul <toda1213@naver.com>
2022-07-17 03:24:31 +0900

    FirstCompose 생성
    첫 예제 코드 생성

--- app/src/main/java/com/example/mvvmrecipeappclone/MainActivity.kt
+++ app/src/main/java/com/example/mvvmrecipeappclone/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.mvvmrecipeappclone
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mvvmrecipeappclone.ui.theme.MVVMRecipeAppCloneTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +23,37 @@ class MainActivity : ComponentActivity() {
         setContent {
             MVVMRecipeAppCloneTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colors.background
+//                ) {
+//                    Greeting("Android")
+//                }
+
+                FirstCompose()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun FirstCompose() {
+    Column(
+        modifier = Modifier.padding(10.dp)
+    ) {
+        Text(text = "Hey look some text")
+        Spacer(modifier = Modifier.padding(top=10.dp))
+        Button(
+            onClick = {}
+        ) {
+            Text(text = "A BUTTON")
+        }
+    }
 }
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MVVMRecipeAppCloneTheme {
-        Greeting("Android")
+        FirstCompose()
     }
 }
