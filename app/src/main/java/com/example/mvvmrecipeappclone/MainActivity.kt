071e298af57321282127f02b135c920cf120cd12
Oneul <toda1213@naver.com>
2022-07-22 23:28:53 +0900

    Column Layouts with Jetpack Compose 완료

--- app/src/main/java/com/example/mvvmrecipeappclone/MainActivity.kt
+++ app/src/main/java/com/example/mvvmrecipeappclone/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -16,8 +18,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mvvmrecipeappclone.ui.theme.MVVMRecipeAppCloneTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,8 +47,10 @@ class MainActivity : ComponentActivity() {
 fun ColumnLayoutCompose() {
     Column(
         modifier = Modifier
-            .fillMaxWidth()
+            .background(color = Color(0xFFF2F2F2))
+            .fillMaxSize()
             .padding(0.dp)
+            .verticalScroll(rememberScrollState())
     ) {
         Image(
             painter = painterResource(
@@ -51,16 +58,32 @@ fun ColumnLayoutCompose() {
             ),
             contentDescription = "",
             contentScale = ContentScale.Crop,
-            modifier = Modifier.height(300.dp)
+            modifier = Modifier.height(300.dp).fillMaxWidth()
         )
         Column(
             modifier = Modifier.padding(16.dp),
         ) {
-            Text(text = "Happy Meal")
+            Text(
+                text = "Happy Meal",
+                style = TextStyle(
+                    fontSize = 26.sp
+                )
+            )
             Spacer(modifier = Modifier.padding(top = 10.dp))
-            Text(text = "800 Calories")
+            Text(
+                text = "800 Calories",
+                style = TextStyle(
+                    fontSize = 17.sp
+                )
+            )
             Spacer(modifier = Modifier.padding(top = 10.dp))
-            Text(text = "$5.99")
+            Text(
+                text = "$5.99",
+                style = TextStyle(
+                    color = Color(0xFF85bb65),
+                    fontSize = 17.sp
+                )
+            )
         }
     }
 
